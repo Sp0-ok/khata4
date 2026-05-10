10752660894e25e93d592e715dbcf3a259f707c0
Sp0-ok <luckysherlock@proton.me>
2026-05-10 12:27:34 +0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -17,6 +17,10 @@ const config: CapacitorConfig = {
       splashFullScreen: true,
       splashImmersive: true,
     },
+    Keyboard: {
+      resize: "body",
+      resizeOnFullScreen: true,
+    },
   },
 };
 
